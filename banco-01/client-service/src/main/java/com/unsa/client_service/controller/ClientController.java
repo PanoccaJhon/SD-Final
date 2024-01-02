c30e1008f04c588cb10fe05788562e2c63b318e8
PanoccaJhon <jpanoccam@unsa.edu.pe>
2024-01-02 10:41:26 -0500

    Api gateway y discovery service configurados

--- banco-01/client-service/src/main/java/com/unsa/client_service/controller/ClientController.java
+++ banco-01/client-service/src/main/java/com/unsa/client_service/controller/ClientController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/clients")
+@RequestMapping("/api/client")
 @RequiredArgsConstructor
 public class ClientController {
     private final ClientService clientService;
